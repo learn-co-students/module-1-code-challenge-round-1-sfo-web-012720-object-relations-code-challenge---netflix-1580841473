1f21b4fac9d5238075a12c454e545ac6b674ff11
JABermudez11 <justinanbermudez@gmail.com>
2020-02-04 11:46:30 -0800

    first code challenege

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,27 @@ class Movie
     @@all
   end
 
+
+
+  def reviews
+    Review.all.select{ |review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map { |review| review.viewer}
+  end
+
+  def ratings
+    reviews.map { |review| review.rating }
+  end
+
+  def average_rating
+    sum = ratings.sum.to_f
+    len = ratings.count.to_f
+    avg = sum / len
+  end
+
+  def self.highest_rated
+    Review.all.max_by{ |review| review.rating }
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -11,9 +11,20 @@ end
 # so they will be available to test and play around with in your console
 
 
+movie1 = Movie.new("Apes")
+movie2 = Movie.new("something")
+movie3 = Movie.new("WhatThe")
 
+viewer1 = Viewer.new("me")
+viewer2 = Viewer.new("ma")
 
+review1 = Review.new(viewer1, movie1, 6)
+review2 = Review.new(viewer2, movie1, 7)
 
+review3 = Review.new(viewer1, movie2, 2)
+review4 = Review.new(viewer2, movie2, 0)
+
+review5 = Review.new(viewer1, movie3, 9)
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
 0
