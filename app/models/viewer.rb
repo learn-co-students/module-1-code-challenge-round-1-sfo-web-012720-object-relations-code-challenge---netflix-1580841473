583b180c9b7a053b1d21d2f76c786b39f06d2c01
Levi Miller <paullevimiller@gmail.com>
2020-02-04 16:02:45 -0800

    Add tests

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -20,14 +20,24 @@ class Viewer
     # end
   end
 
-  def rate_movie(movie,rating)
-    if viewer.movie != self
-      Review.new(self, movie, rating)
+  # - if the `Viewer` instance and the passed `Movie` instance are _not_ already associated, 
+  # this method should create a new `Review` instance
+  # - if this `Viewer` has already reviewed this `Movie`, 
+  # assigns the new rating to the existing `Review` instance
+  def rate_movie(movie, rating)
+
+    viewer_reviews = Review.all.select{ |review| review.viewer == self }
+    viewer_movie_review = viewer_reviews.find{ |review| review.movie == movie}
+    # viewer_movies = viewer_reviews.map{ |review| review.movie }
+
+    if viewer_movie_review
+      viewer_movie_review.rating = rating
     else
-      ## code here
+      Review.new(self, movie, rating)
     end
   end
 
-
- 
+  def reviews
+    Review.all.select{ |review| review.viewer == self }
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -9,16 +9,16 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-Review.new("Viewer_Dan","Terminator",10)
-Review.new("Viewer_Jake","Predator",9)
-Review.new("Viewer_Zach","Avengers",7)
-Movie.new("Terminator2")
-Movie.new("Batman")
-Movie.new("Ace_Ventua")
-Viewer.new("User_Bob")
-Viewer.new("User_Rod")
-Viewer.new("User_Jane")
+term = Movie.new("Terminator2")
+bat = Movie.new("Batman")
+ace = Movie.new("Ace_Ventua")
+bob = Viewer.new("User_Bob")
+rod = Viewer.new("User_Rod")
+jane = Viewer.new("User_Jane")
 
+Review.new(bob, term, 10)
+Review.new(rod, bat, 9)
+Review.new(jane, ace, 7)
 
 
 
